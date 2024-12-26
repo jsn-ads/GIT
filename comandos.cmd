a67a50e629fc682812c97cb9358ff44cb641dbe4
Neto Home <jsn.ads@gmail.com>
2024-12-26 17:11:00 -0300

    GIT - Inicializando

--- comandos.cmd
+++ comandos.cmd
@@ -1,3 +1,6 @@
+================================================CONFIGURAÇÃO==============================================
+
+
 @REM 1. Verifica versão
         git --version
 
@@ -11,3 +14,11 @@
         @REM um item
         git config --get user.name
         git config --get user.email
+
+==================================================INSTALAÇÃO==============================================
+
+@REM 1. Criar GIT no projeto
+        git init
+
+@REM 2 Verificar se existe um GIT no projeto
+        git status
