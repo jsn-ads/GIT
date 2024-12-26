040f3c571e033cb2f64da47ab88d8453bd55ee5b
Neto Home <jsn.ads@gmail.com>
2024-12-26 16:55:11 -0300

    GIT - Configurações

--- comandos.cmd
+++ comandos.cmd
@@ -1,2 +1,13 @@
-@REM 1. verifica versão
+@REM 1. Verifica versão
         git --version
+
+@REM 2. Criando usuario git no projeto (boa pratica cria usuario e email os mesmo utilizados no github)
+        git config --global user.name "exemplo"
+        git config --global user.email "exemplo@exemplo"
+
+@REM 3. Verifica usuario e email configurados
+        @REM full dados
+        git config --list
+        @REM um item
+        git config --get user.name
+        git config --get user.email
